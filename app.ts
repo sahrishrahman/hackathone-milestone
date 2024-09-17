02ef54d12a9e73754710ad7b3f3805aa527f1e88
sahrishrahman <160232872+sahrishrahman@users.noreply.github.com>
2024-09-17 08:19:00 -0700

    Add files via upload

--- app.ts
+++ app.ts
@@ -18,20 +18,20 @@ if (form) {
 
     // Generate the resume content dynamically
     const resumeHtml = `
-      <h2><b>Editable Resume</b></h2>
+      <h2><b>Resume</b></h2>
       <h3>Personal Information</h3>
-      <p><b>Name:</b><span contenteditable="true"> ${name}</span></p>
-      <p><b>Email:</b><span contenteditable="true"> ${email}</span></p>
-      <p><b>Phone:</b><span contenteditable="true"> ${phone}</span></p>
+      <p><b>Name:</b> ${name}</p>
+      <p><b>Email:</b> ${email}</p>
+      <p><b>Phone:</b> ${phone}</p>
 
       <h3>Education</h3>
-      <p contenteditable="true">${education}</p>
+      <p>${education}</p>
 
       <h3>Experience</h3>
-      <p contenteditable="true">${experience}</p>
+      <p>${experience}</p>
 
       <h3>Skills</h3>
-      <p contenteditable="true">${skills}</p>
+      <p>${skills}</p>
     `;
 
     // Display generated resume
